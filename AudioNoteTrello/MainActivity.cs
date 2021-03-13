e345b8aa64f28f3ee5911db9f335f780353af67b
Roberta Chianese <ro.chianese@gmail.com>
2021-03-13 08:09:32 +0000

    Remove unuse param and add permissions

--- AudioNoteTrello/MainActivity.cs
+++ AudioNoteTrello/MainActivity.cs
@@ -50,7 +50,7 @@ namespace AudioNoteTrello
             SetSecretButtonClick(Resource.Id.setTrelloListId, "TrelloListId", apiText);
             SetSecretButtonClick(Resource.Id.setTrelloApiToken, "TrelloApiToken", apiText);
         }
-        
+
         async void RecordButtonTouch(object sender, View.TouchEventArgs e)
         {
             switch (e.Event?.Action & MotionEventActions.Mask)
@@ -63,10 +63,11 @@ namespace AudioNoteTrello
                     }
                     else
                         PerformRuntimePermissionsCheckForRecording(this);
+
                     break;
 
                 case MotionEventActions.Up:
-                    await StopRecording(sender);
+                    await StopRecording();
                     break;
 
                 default:
@@ -99,7 +100,7 @@ namespace AudioNoteTrello
             _recorder.Start();
         }
 
-        async Task StopRecording(object sender)
+        async Task StopRecording()
         {
             if (_recorder == null)
                 return;
@@ -110,33 +111,39 @@ namespace AudioNoteTrello
 
             if (File.Exists(GetFileNameForRecording(this, Ext)))
                 await AudioNoteProcessor.ProcessAsync(GetFileNameForRecording(this, Ext),
-                                                      msg => _logView.Text += "\n" + msg);
+                    msg => _logView.Text += "\n" + msg);
         }
 
         static void Message(object sender, string text) =>
             Snackbar.Make((View) sender, text, BaseTransientBottomBar.LengthLong)
-                    .SetAction("Action", (View.IOnClickListener) null)
-                    .Show();
+                .SetAction("Action", (View.IOnClickListener) null)
+                .Show();
 
         public static readonly string[] RequiredPermissions =
-            { Manifest.Permission.WriteExternalStorage, Manifest.Permission.RecordAudio };
+        {
+            Manifest.Permission.WriteExternalStorage,
+            Manifest.Permission.ReadExternalStorage,
+            Manifest.Permission.RecordAudio,
+            Manifest.Permission.Internet
+        };
+
         public static readonly int RequestAllPermissions = 1200;
 
-        public static bool HasPermissionToRecord(Context context) => 
+        public static bool HasPermissionToRecord(Context context) =>
             !RequiredPermissions.Select(permission => ContextCompat.CheckSelfPermission(context, permission))
-                                .Contains(Permission.Denied);
+                .Contains(Permission.Denied);
 
         public static void PerformRuntimePermissionsCheckForRecording(Activity activity)
         {
             if (ShouldShowUserPermissionRationle(activity))
             {
-                var rationale = 
+                var rationale =
                     Snackbar.Make(GetLayoutForSnackbar(activity),
-                                  "Resource.String.permissions_rationale pippopeppe",
-                                  BaseTransientBottomBar.LengthIndefinite);
+                        "Resource.String.permissions_rationale pippopeppe",
+                        BaseTransientBottomBar.LengthIndefinite);
 
                 rationale.SetAction(
-                    "Oki", 
+                    "Oki",
                     obj => ActivityCompat.RequestPermissions(activity, RequiredPermissions, RequestAllPermissions));
 
                 rationale.Show();
@@ -145,15 +152,15 @@ namespace AudioNoteTrello
                 ActivityCompat.RequestPermissions(activity, RequiredPermissions, RequestAllPermissions);
         }
 
-        public static bool ShouldShowUserPermissionRationle(Activity activity) => 
+        public static bool ShouldShowUserPermissionRationle(Activity activity) =>
             ActivityCompat.ShouldShowRequestPermissionRationale(activity, Manifest.Permission.RecordAudio) &&
             ActivityCompat.ShouldShowRequestPermissionRationale(activity, Manifest.Permission.WriteExternalStorage);
 
-        public static View GetLayoutForSnackbar(Activity activity) => 
+        public static View GetLayoutForSnackbar(Activity activity) =>
             activity.FindViewById(Android.Resource.Id.Content);
-        
-        public static string GetFileNameForRecording(Context context, string ext) => 
+
+        public static string GetFileNameForRecording(Context context, string ext) =>
             Path.Combine(context.GetExternalFilesDir(Environment.DirectoryMusic)!.AbsolutePath,
-                         "note." + ext);
+                "note." + ext);
     }
 }
